72d16ce0136139de012b1d414bfddcd281a893bb
KIMINJOONG <ijboym216@gmail.com>
2022-01-20 14:28:30 +0900

    make InkWell button

--- lib/components/profile_buttons.dart
+++ lib/components/profile_buttons.dart
@@ -5,6 +5,7 @@ class ProfileButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         _buildFollowButton(),
         _buildMessageButton()
@@ -13,10 +14,40 @@ class ProfileButtons extends StatelessWidget {
   }
 
   Widget _buildFollowButton() {
-    return SizedBox();
+    return InkWell(
+      onTap: () {
+        print("Follow 버튼 클릭됌");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        child: Text(
+          "Follow",
+          style: TextStyle(color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+    );
   }
 
   Widget _buildMessageButton() {
-    return SizedBox();
+    return Container(
+      alignment: Alignment.center,
+      width: 150,
+      height: 45,
+      child: Text(
+        "Message",
+        style: TextStyle(color: Colors.black),
+      ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        border: Border.all(),
+      ),
+    );
   }
 }
